955eee63b58cbe6abd16adef67a6346889139332
Alexander Dolgikh <alexander.dolgikh@corp.mail.ru>
2020-10-11 22:33:34 +0300

    added tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,34 @@
+import Test.Hspec (describe, hspec, it, shouldBe)
+import Text.Megaparsec (parse)
+
+import Python.AST
+import Python.Parser
+
+exampleSource = unlines
+    [ "x = 5"
+    , "y = 6"
+    , "temp = x"
+    , "x = y"
+    , "y = temp"
+    , "xy = x + y"
+    , "print(xy)"
+    , "def sumnums (f, s):"
+    , "   r = f + s"
+    , "   return r"
+    ]
+
+exampleDSL = do
+    command . Assign "x" . Literal $ VInt 5
+    command . Assign "y" . Literal $ VInt 6
+    command . Assign "temp" $ Variable "x"
+    command . Assign "x" $ Variable "y"
+    command . Assign "y" $ Variable "temp"
+    command . Assign "xy" $ Binop Add (Variable "x") (Variable "y")
+    command . Expression $ Call "print" [Variable "xy"]
+    command . Define "sumnums" ["f", "s"] $ do
+        command . Assign "r" $ Binop Add (Variable "f") (Variable "s")
+        command . Return $ Variable "r"
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec . describe "parser" . it "parsing source code" $
+    parse python "" exampleSource `shouldBe` Right exampleDSL
